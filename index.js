8e1620e3ee991c5f0ebd760c99bfa3c3dd6b9026
Mykola Kolisnyk <kolesnikny@gmail.com>
2022-01-15 20:14:17 +0200

    add task 4

--- index.js
+++ index.js
@@ -28,31 +28,52 @@ function createRandomUser(quantity) {
   return res;
 }
 
+const userArr = createRandomUser(50);
+console.table(userArr);
+
 //1. Получить массив полных имен с помощью .map()
-const getFullNamesArr = (users) => users.map((item) => item.getFullName());
+
+const getFullNames = (users) => users.map((item) => item.getFullName());
+const allUsersNames = getFullNames(userArr);
+//console.log(allUsersNames);
 
 //2. Получить массив совершеннолетних пользователей
-const getAdultUsersArr = (users) =>
+
+const getAdultUsers = (users) =>
   users.filter((item) => {
     if (item.age > 18) {
-      return this;
+      return item;
     }
   });
+
+const allAdultUsers = getAdultUsers(userArr);
+//console.log(allAdultUsers);
+
 //3. Зарандомить подписку у пользователей
-const setRandomSubscribe = (users) =>
+
+const setRandomSubscribes = (users) =>
   users.forEach((item) => (item.isSubscribe = Math.random() > 0.5));
+
+setRandomSubscribes(userArr);
+//console.table(userArr);
+
 //4. Получить массив подписанных совершеннолетних пользователей женского пола, но вывести только их почту
 
-const userArr = createRandomUser(50);
-//console.table(userArr);
-//1.
-const allUsersNames = getFullNamesArr(userArr);
-//console.log(allUsersNames);
-//2.
-const allAdultUsers = getAdultUsersArr(userArr);
-//console.log(allAdultUsers);
-//3.
+const getSubscribeUsers = (users) =>
+  users.filter((item) => {
+    if (item.isSubscribe) {
+      return item;
+    }
+  });
 
-setRandomSubscribe(userArr);
-//4.
-console.table(userArr);
+const getFamaleUsers = (users) =>
+  users.filter((item) => {
+    if (!item.isMale) {
+      return item;
+    }
+  });
+
+const allAdultFamaleUsers = getSubscribeUsers(
+  getFamaleUsers(getAdultUsers(userArr))
+);
+allAdultFamaleUsers.forEach((item) => console.log(item.email));
